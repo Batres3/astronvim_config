80d3518bc9b3d1880a3be8cd445d2490c359eb76
Javier Batres <batres@pop-os.localdomain>
2023-09-13 15:00:27 +0200

    Main plugins up and running (debugger still not working)

--- mappings.lua
+++ mappings.lua
@@ -6,6 +6,12 @@
 return {
   -- first key is the mode
   n = {
+    ["J"] = { "mzJ`z", desc = "Normal J but cursor stays at same spot" },
+    ["<C-d>"] = { "<C-d>zz", desc = "Move down while cursor stays in the middle of the screen"},
+    ["<C-u>"] = { "<C-u>zz", desc = "Move up while cursor stays in the middle of the screen" },
+    ["n"] = { "nzzzv", desc = "When cycling through selections cursor stays in the middle of the screen"},
+    ["N"] = { "Nzzzv", desc = "When cycling through selections cursor stays in the middle of the screen" },
+    ["<leader>s"] = { ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", desc = "Find and replace current word" },
     -- second key is the lefthand side of the map
 
     -- navigate buffer tabs with `H` and `L`
@@ -30,8 +36,17 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    ["<leader>r"] = { name = "Run Code"},
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+  },
+  v = {
+    ["J"] = { ":m '>+1<CR>gv=gv", desc = "Move selection down" },
+    ["K"] = { ":m '<-2<CR>gv=gv", desc = "Move selection down" },
+    ["<leader>d"] = { "\"_d", desc = "Delete into void register"},
+  },
+  x = {
+    ["<leader>p"] = { "\"_dP", desc = "Paste selection into void register"},
   },
   t = {
     -- setting a mapping to false will disable it
