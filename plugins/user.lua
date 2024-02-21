805873658dfa01c857cbe3f2b1af2d1e2e6259d3
Javier Batres <batres@pop-os.localdomain>
2024-02-21 19:32:40 +0100

    Small changes

--- plugins/user.lua
+++ plugins/user.lua
@@ -113,4 +113,20 @@ return {
       move_cursor_key = nil, -- imap, use nvim_set_current_win to move cursor between current win and floating
     },
   },
+  {
+    'linux-cultist/venv-selector.nvim',
+    dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
+    opts = {
+      -- Your options go here
+      -- name = "venv",
+      -- auto_refresh = false
+    },
+    event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+    keys = {
+      -- Keymap to open VenvSelector to pick a venv.
+      { '<leader>vs', '<cmd>VenvSelect<cr>' },
+      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+      { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
+    },
+  }
 }
